5e0aed0dab22d974485309ec216bc7c15f05758f
goncasmage <goncalo.alex.marx@gmail.com>
2019-07-16 20:02:03 +0100

    Add NavText nullcheck to UINavSliderBox

--- Source/UINavigation/Private/UINavWidget.cpp
+++ Source/UINavigation/Private/UINavWidget.cpp
@@ -1160,20 +1160,12 @@ void UUINavWidget::SwitchTextColorTo(int Index, FLinearColor Color)
 	if (NewComponentIndex != -1)
 	{
 		NewText = UINavComponents[NewComponentIndex]->NavText;
-		if (NewText == nullptr)
-		{
-			DISPLAYERROR("When UseTextColor is true, UINavComponent should have a valid TextBlock called NavText.");
-			return;
-		}
+		if (NewText == nullptr) return;
 	}
 	else
 	{
 		NewText = Cast<UTextBlock>(UINavButtons[Index]->GetChildAt(0));
-		if (NewText == nullptr)
-		{
-			DISPLAYERROR("When UseTextColor is true, UINavButton should have a TextBlock as its child.");
-			return;
-		}
+		if (NewText == nullptr) return;
 	}
 	NewText->SetColorAndOpacity(Color);
 }
