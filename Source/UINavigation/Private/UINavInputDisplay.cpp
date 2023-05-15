bbf15d3c88d53fa65e7ce5092d475631bc16c363
Gonçalo Marques <goncalo.alex.marx@gmail.com>
2023-05-15 07:42:39 +0100

    Add UINavInputDisplay class

---
+++ Source/UINavigation/Private/UINavInputDisplay.cpp
@@ -0,0 +1,112 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UINavInputDisplay.h"
+#include "GameFramework/PlayerController.h"
+#include "UINavPCComponent.h"
+#include "Engine/Texture2D.h"
+#include "Components/Image.h"
+#include "Components/TextBlock.h"
+
+void UUINavInputDisplay::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	APlayerController* PC = Cast<APlayerController>(GetOwningPlayer());
+	if (!IsValid(PC))
+	{
+		return;
+	}
+
+	UINavPC = PC->FindComponentByClass<UUINavPCComponent>();
+	if (!IsValid(UINavPC))
+	{
+		return;
+	}
+
+	UINavPC->InputTypeChangedDelegate.AddDynamic(this, &UUINavInputDisplay::InputTypeChanged);
+
+	UpdateInputVisuals();
+}
+
+void UUINavInputDisplay::NativeDestruct()
+{
+	if (!IsValid(UINavPC))
+	{
+		return;
+	}
+
+	UINavPC->InputTypeChangedDelegate.RemoveDynamic(this, &UUINavInputDisplay::InputTypeChanged);
+
+	Super::NativeDestruct();
+}
+
+void UUINavInputDisplay::NativePreConstruct()
+{
+	Super::NativePreConstruct();
+
+	if (IsValid(InputText))
+	{
+		if (bOverride_Font)
+		{
+			InputText->SetFont(FontOverride);
+		}
+		else
+		{
+			FontOverride = InputText->Font;
+		}
+
+		if (bOverride_TextColor)
+		{
+			InputText->SetColorAndOpacity(TextColorOverride);
+		}
+		else
+		{
+			TextColorOverride = InputText->ColorAndOpacity;
+		}
+	}
+
+	if (IsValid(InputImage) && !bMatchIconSize)
+	{
+		InputImage->SetBrushSize(IconSize);
+	}
+}
+
+void UUINavInputDisplay::UpdateInputVisuals()
+{
+	if (!IsValid(UINavPC))
+	{
+		return;
+	}
+
+	UTexture2D* NewTexture = UINavPC->GetEnhancedInputIcon(InputAction, Axis, Scale);
+	if (NewTexture != nullptr)
+	{
+		InputImage->SetBrushFromTexture(NewTexture, bMatchIconSize);
+		if (!bMatchIconSize)
+		{
+			InputImage->SetBrushSize(IconSize);
+		}
+
+		InputText->SetVisibility(ESlateVisibility::Collapsed);
+	}
+	else
+	{
+		InputText->SetText(UINavPC->GetEnhancedInputText(InputAction, Axis, Scale));
+		InputImage->SetVisibility(ESlateVisibility::Collapsed);
+	}
+}
+
+void UUINavInputDisplay::InputTypeChanged(const EInputType NewInputType)
+{
+	UpdateInputVisuals();
+}
+
+void UUINavInputDisplay::SetInputAction(UInputAction* NewAction, const EInputAxis NewAxis, const EAxisType NewScale)
+{
+	InputAction = NewAction;
+	Axis = NewAxis;
+	Scale = NewScale;
+
+	UpdateInputVisuals();
+}
---
+++ Source/UINavigation/Public/UINavInputDisplay.h
@@ -0,0 +1,85 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Blueprint/UserWidget.h"
+#include "InputAction.h"
+#include "Data/InputType.h"
+#include "Data/AxisType.h"
+#include "Data/InputContainerEnhancedActionData.h"
+#include "Math/Vector2D.h"
+#include "UINavInputDisplay.generated.h"
+
+class UImage;
+class UTextBlock;
+class UUINavPCComponent;
+
+/**
+ * 
+ */
+UCLASS()
+class UINAVIGATION_API UUINavInputDisplay : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+
+	virtual void NativeConstruct() override;
+	virtual void NativeDestruct() override;
+
+	virtual void NativePreConstruct() override;
+
+	UFUNCTION(BlueprintCallable, Category = "InputDisplay")
+	void UpdateInputVisuals();
+
+	UFUNCTION(BlueprintCallable, Category = "InputDisplay")
+	void SetInputAction(UInputAction* NewAction, const EInputAxis NewAxis, const EAxisType NewScale);
+
+protected:
+
+	UFUNCTION()
+	void InputTypeChanged(const EInputType NewInputType);
+
+public:
+
+	UPROPERTY(BlueprintReadWrite, meta = (BindWidget), Category = "InputDisplay")
+	UImage* InputImage = nullptr;
+
+	UPROPERTY(BlueprintReadOnly, meta = (BindWidget), Category = "InputDisplay")
+	UTextBlock* InputText = nullptr;
+
+protected:
+
+	UPROPERTY(EditAnywhere, Category = "InputDisplay")
+	UInputAction* InputAction = nullptr;
+
+	UPROPERTY(EditAnywhere, Category = "InputDisplay")
+	EInputAxis Axis = EInputAxis::X;
+	
+	UPROPERTY(EditAnywhere, Category = "InputDisplay")
+	EAxisType Scale = EAxisType::None;
+
+	UPROPERTY(EditAnywhere, Category = "InputDisplay")
+	bool bMatchIconSize = false;
+
+	UPROPERTY(EditAnywhere, Category = "InputDisplay", meta = (editcondition = "!bMatchIconSize"))
+	FVector2D IconSize = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = UINavComponent, meta = (editcondition = "bOverride_TextColor"))
+	FSlateColor TextColorOverride;
+
+	UPROPERTY(EditAnywhere, Category = UINavComponent, meta = (InlineEditConditionToggle))
+	uint8 bOverride_TextColor : 1;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = UINavComponent, meta = (editcondition = "bOverride_Font"))
+	FSlateFontInfo FontOverride;
+
+	UPROPERTY(EditAnywhere, Category = UINavComponent, meta = (InlineEditConditionToggle))
+	uint8 bOverride_Font : 1;
+
+private:
+
+	UUINavPCComponent* UINavPC = nullptr;
+	
+};
+ 
