10494faa3817a2b1711f9d9e8588d8d2ad12bcc4
Gonçalo Marques <goncalo.alex.marx@gmail.com>
2026-06-09 07:11:06 +0100

    Fix pressed button state not showing when not using Focus System Inputs

--- Source/UINavigation/Private/UINavWidget.cpp
+++ Source/UINavigation/Private/UINavWidget.cpp
@@ -2220,6 +2220,11 @@ void UUINavWidget::OnPressedComponent(UUINavComponent* Component)
 
 	SelectCount++;
 
+	if (!GetDefault<UUINavSettings>()->bUseFocusSystemNavigationInputs)
+	{
+		Component->SwitchButtonStyle(Component->NavButton->IsPressed() || SelectCount > 0 ? EButtonStyle::Pressed : (Component == CurrentComponent ? EButtonStyle::Hovered : EButtonStyle::Normal));
+	}
+
 	PropagateOnStartSelect(CurrentComponent);
 }
 
@@ -2235,7 +2240,7 @@ void UUINavWidget::OnReleasedComponent(UUINavComponent* Component)
 
 	if (IsValid(Component))
 	{
-		Component->SwitchButtonStyle(Component->NavButton->IsPressed() || SelectCount > 1 ? EButtonStyle::Pressed : (Component == CurrentComponent ? EButtonStyle::Hovered : EButtonStyle::Normal));
+		Component->SwitchButtonStyle(Component == CurrentComponent ? EButtonStyle::Hovered : EButtonStyle::Normal);
 
 		if (SelectCount > 0) SelectCount--;
 		if (SelectCount == 0)
